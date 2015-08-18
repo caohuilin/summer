27c94c959312fc66353f1aa894aa6293cd6d6561
caohuilin <chlcaohuilin@qq.com>
2015-08-18 19:13:50 +0800

    js实现下拉菜单

---
+++ xiala_menu/js_shixan/js/main.js
@@ -0,0 +1,9 @@
+function ShowSub(li) {
+      var subMenu =  li.getElementsByTagName("ul")[0]  ;
+      subMenu.style.display = "block ";
+        }
+
+function HideSub(li) {
+    var subMenu = li.getElementsByTagName("ul")[0];
+    subMenu.style.display = "none";
+        }
