738c77695583fdc20ee1c6a4002612b2905aa14e
caohuilin <chlcaohuilin@qq.com>
2015-08-23 00:46:17 +0800

    侧边栏 菜单 实现 完成最后动画效果

--- cebian_menu/html+css/js/main.js
+++ cebian_menu/html+css/js/main.js
@@ -13,16 +13,40 @@
         var menuContentEl = document.getElementById(e.currentTarget.id+"-content");
         if(self.state === "allClosed"){
           console.log("打开"+menuContentEl.id);
+          menuContentEl.style.top = "0";
+          menuContentEl.style.left = "-85px";
+          menuContentEl.className = "nav-content";
+          menuContentEl.classList.add("menuContent-move-right");
           self.state = "hasOpened";
           self.currentOpendMenuContent = menuContentEl;
         }
         else{
           console.log("关闭"+self.currentOpendMenuContent.id);
+          self.currentOpendMenuContent.style.top = "0";
+          self.currentOpendMenuContent.style.left = "35px";
+          self.currentOpendMenuContent.className = "nav-content";
+          self.currentOpendMenuContent.classList.add("menuContent-move-left");
           console.log("打开"+menuContentEl.id);
+          menuContentEl.style.top = "250px";
+          menuContentEl.style.left = "35px";
+          menuContentEl.className = "nav-content";
+          menuContentEl.classList.add("menuContent-move-up");
           self.state = "hasOpened";
           self.currentOpendMenuContent = menuContentEl;
         }
       });
+    }
+    this.menuContentList = document.querySelectorAll(".nav-content >div.nav-con-close");
+    for(var i=0;i<this.menuContentList.length;i++){
+      this.menuContentList[i].addEventListener("click",function(e){
+        var menuContent = e.currentTarget.parentNode;
+        menuContent.style.top = "0";
+        menuContent.style.left = "35px";
+        menuContent.className = "nav-content";
+        menuContent.classList.add("menuContent-move-left");
+        this.state = "allClosed";
+      })
+
     }
   };
   var Sidebar = function(eId,closeBarId){
@@ -45,6 +69,10 @@
   };
   Sidebar.prototype.open = function(){
       console.log("打开");
+      this.el.style.left = "-120px";
+      this.el.className = "sidebar-move-right";
+      this.closeBarEl.style.left = "160px";
+      this.closeBarEl.className = "closeBar-move-left";
       this.state = "opened";
   };
   Sidebar.prototype.triggerSwich = function(){
