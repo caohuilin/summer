9560819a1d64f7eddb23cdacda23832503b64e20
caohuilin <chlcaohuilin@qq.com>
2015-08-24 18:01:57 +0800

    伸缩型广告的实现

--- guanggao/right_bottom/js/main.js
+++ guanggao/right_bottom/js/main.js
@@ -1,8 +1,14 @@
 window.onload = function(){
   var TipBox = document.getElementById('tipCon');
-  var CloseBtn = document.getElementById('closeBtn');
-
-  CloseBtn.onclick = function(){
-    TipBox.className = "hide";
+  var ClickMe = document.getElementById('clickMe');
+  var showPic = document.getElementById('showPic');
+  var closeBtn = document.getElementById('closeBtn');
+  ClickMe.onmouseover = function(){
+    showPic.style.display = "block";
+    closeBtn.style.display = "block";
+  }
+  closeBtn.onclick = function(){
+    showPic.style.display = "none";
+    closeBtn.style.display = "none";
   }
 }
