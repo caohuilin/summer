3955e954d59a405a71b78a5af8916ea757cac8f4
caohuilin <chlcaohuilin@qq.com>
2015-12-28 21:39:04 +0800

    logbook 主页数据的实现和侧边栏基本显示的实现

--- logbook/js/main.js
+++ logbook/js/main.js
@@ -1,15 +1,94 @@
 ;(function($){
 
 
-  //部门
+  //部门 以及每个部门中的人
   $.get("http://96a8to7r.apps.qbox.me/departments",function(departments){
     //console.log(departments.data);
-    var html="";
-    $.each(departments.data,function(i,dep){
-    //  console.log(dep);
-      html+="<li>"+dep+"</li>";
+    var depart = {};
+    var us_ht = {};
+    var name_ht = {};
+    $.get("http://96a8to7r.apps.qbox.me/users",function(users){
+    //  console.log(users);
+      $.each(users.data,function(i,us){
+        if(depart[us.department]){
+          depart[us.department]++;
+          us_ht[us.department] += "<li><div class='name'>姓名："+us.real_name+"</div><div class='mood'>心情：</div><div class='note'>日志:</div></li>";
+          name_ht[us.department] += "<li>"+us.real_name+"</li>";
+        }else{
+          depart[us.department] = 1;
+          us_ht[us.department] = "<li><div class='name'>姓名："+us.real_name+"</div><div class='mood'>心情：</div><div class='note'>日志:</div></li>";
+          name_ht[us.department] = "<li>"+us.real_name+"</li>";
+        }
+      });
+    //  console.log(name_ht);
+      var html = "";
+      var html2 = "";
+      $.each(departments.data,function(i,dep){
+       if(!depart[dep]){
+         depart[dep]=0;
+           html += '<li>'+dep+'<div class="num">共'+depart[dep]+'人</div></li><ul class="gs"></ul>';
+            html2 += '<li>'+dep+'('+depart[dep]+'/'+depart[dep]+'人)</div></li><ul class="gd"></ul>';
+       }else{
+        html += '<li>'+dep+'<div class="num">共'+depart[dep]+'人</div></li><ul class="gs">'+us_ht[dep]+'</ul>';
+         html2 += '<li>'+dep+'('+depart[dep]+'/'+depart[dep]+'人)</div></li><ul class="gd">'+name_ht[dep]+'</ul>';
+      }
+      });
+      $(".departments").html(html);
+      $(".c_de").html(html2);
+      //每个部门条目的点击事件
+      var show = -1;
+      var flag = 1;
+      var de = $(".departments >li");
+      var us = $(".gs");
+      $.each(de,function(i,d){
+        de.eq(i).on("click", function(){
+        if(show != -1){
+           us.eq(show).css("display","none");
+         }
+        if(show == i){
+          if(flag){
+            us.eq(show).css("display","none");
+            flag = 0;
+          }
+          else{
+            us.eq(show).css("display","block");
+            flag = 1;
+          }
+        }else{
+           show = i;
+            us.eq(i).css("display","block");
+            flag = 1;
+          }
+        });
+      });
+
+      //左边区域的点击事件
+      var show2 = -1;
+      var flag2 = 1;
+      var de2 = $(".c_de >li");
+      var us2 = $(".gd");
+      $.each(de2,function(i,d){
+        de2.eq(i).on("click", function(){
+        if(show2 != -1){
+           us2.eq(show2).css("display","none");
+         }
+        if(show2 == i){
+          if(flag2){
+            us2.eq(show2).css("display","none");
+            flag2 = 0;
+          }
+          else{
+            us2.eq(show2).css("display","block");
+            flag2 = 1;
+          }
+        }else{
+           show2= i;
+            us2.eq(i).css("display","block");
+            flag2 = 1;
+          }
+        });
+      });
     });
-    $(".departments").html(html);
   });
 
   //个人信息
@@ -43,5 +122,11 @@
     $("#mask").hide();
     $("#popup").hide();
   });
+
+  $('.date div').datepicker({
+    format:'yyyy-mm-dd',
+    todayHighlight:true
+  });
+
 
 })(jQuery);
