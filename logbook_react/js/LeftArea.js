e524ef972366a577d544f4f672a2dc77fbc16a6c
caohuilin <1057275848@qq.com>
2016-01-22 21:15:40 +0800

    添加日历组件  通过创建一个div，通过jquery的方法添加到calender组件中

--- logbook_react/js/LeftArea.js
+++ logbook_react/js/LeftArea.js
@@ -1,8 +1,27 @@
+//日历组件
+var Calender = React.createClass({
+  componentDidMount:function(){
+    var cal = this.cal = $("<div/>")
+    cal.datepicker({
+      format:'yyyy-mm-dd',
+      todayHighlight:true
+    });
+    var node = ReactDOM.findDOMNode(this);
+    $(node).append(cal);
+  },
+  render:function(){
+    return(
+        <div className="date">
+        </div>
+    );
+  }
+});
 //LeftArea组件
 var LeftArea = React.createClass({
   render:function(){
       return(
         <div className="leftArea">
+          <Calender />
         </div>
       );
   }
--- logbook_react/js/main.js
+++ logbook_react/js/main.js
@@ -1,6 +1,4 @@
 
-
-
 //加载最终页面 两个组件Header和Content
 ReactDOM.render(
   <div className="main" >
