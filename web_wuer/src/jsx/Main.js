b69e638a048070db79816989ea8ab67d557a151f
caohuilin <1057275848@qq.com>
2016-02-08 20:03:13 +0800

    web_wuer

---
+++ web_wuer/src/jsx/Main.js
@@ -0,0 +1,6 @@
+ReactDOM.render(
+    <div className="body">
+        <MainBody/>
+    </div>,
+    document.getElementById('main')
+);
---
+++ web_wuer/src/jsx/MainBody.js
@@ -0,0 +1,17 @@
+const MainBody = React.createClass({
+    render(){
+        return (
+            <div className="main">
+                <div className="cover"></div>
+                <header className="header">
+                    <div className="logo">
+                        <img src="./public/img/logo.png" alt="物联二班"/>
+                    </div>
+                </header>
+                <div className="login_box">
+
+                </div>
+            </div>
+        )
+    }
+});
